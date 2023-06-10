89af4547fca3a5d93e6ac0f0bbb76bf6dc863398
Pratama89 <pratama.adiciptawan@gmail.com>
2023-06-10 08:24:16 +0700

    membuat Portfolio Pharmadent

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
+const { transform } = require("sucrase");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#1d4ed8",
+        dark: "#0f172a",
+      },
+    },
   },
   plugins: [],
 };
-
