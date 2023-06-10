af32eac869589a883200617da701c4b7ef8d1e45
Pratama89 <pratama.adiciptawan@gmail.com>
2023-06-10 16:30:17 +0700

    Membuat Section Navigasi fixed

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,17 @@
+const { isFunctionScopedDeclaration } = require("sucrase/dist/types/parser/tokenizer");
+
+// Navbar Fixed
+window.onscroll = function() {
+    const header = document.querySelector('header');
+    const fixedNav = header.offsetTop;
+
+    if (window.pageYOffset > fixedNav) {
+        header.classList.add('navbar-fixed');
+    } else {
+        header.classList.remove('navbar-fixed');
+    }
+}
+
 // Humberger
 const hamberger = document.querySelector('#hamberger');
 
