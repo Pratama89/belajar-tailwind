375a06effd0cb0d819ed5cba8cec61e45de5d334
Pratama89 <pratama.adiciptawan@gmail.com>
2023-06-10 23:31:21 +0700

    membuat navbar dan apa

--- public/js/script.js
+++ public/js/script.js
@@ -1,20 +1,22 @@
-const { isFunctionScopedDeclaration } = require("sucrase/dist/types/parser/tokenizer");
+// const { isFunctionScopedDeclaration } = require("sucrase/dist/types/parser/tokenizer");
 
 // Navbar Fixed
-window.onscroll = function() {
-    const header = document.querySelector('header');
-    const fixedNav = header.offsetTop;
+window.onscroll = function () {
+  const header = document.querySelector("header");
+  const fixedNav = header.offsetTop;
 
-    if (window.pageYOffset > fixedNav) {
-        header.classList.add('navbar-fixed');
-    } else {
-        header.classList.remove('navbar-fixed');
-    }
-}
+  if (window.pageYOffset > fixedNav) {
+    header.classList.add("navbar-fixed");
+  } else {
+    header.classList.remove("navbar-fixed");
+  }
+};
 
 // Humberger
-const hamberger = document.querySelector('#hamberger');
+const hamberger = document.querySelector("#hamberger");
+const navMenu = document.querySelector("#nav-menu");
 
-hamberger.addEventListener('click', function() {
-    hamberger.classList.toggle('hamberger-active');
+hamberger.addEventListener("click", function () {
+  hamberger.classList.toggle("hamberger-active");
+  navMenu.classList.toggle('hidden')
 });
