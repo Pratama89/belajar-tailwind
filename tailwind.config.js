da7815a21f4a4827f482c504b3a7627b6aaef3bc
Pratama89 <pratama.adiciptawan@gmail.com>
2023-06-10 11:43:37 +0700

    Membuat Section Pharmadent

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,17 @@ const { transform } = require("sucrase");
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
+    container: {
+      center: true,
+      padding: '16px',      
+    },
     extend: {
       colors: {
         primary: "#1d4ed8",
         dark: "#0f172a",
+      },
+      screens: {
+        '2xl': '1320px',
       },
     },
   },
